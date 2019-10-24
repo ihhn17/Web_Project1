0bcce7add6719a2e51c259d2855aff283b1126ce
Ihor Hnat <48947826+ihhn17@users.noreply.github.com>
2019-10-24 03:36:04 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+(function($) {
+   
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$('#slides').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+	
+	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
+	
+	/* ..............................................
+    Special Menu
+    ................................................. */
+	
+	var Container = $('.container');
+	Container.imagesLoaded(function () {
+		var portfolio = $('.special-menu');
+		portfolio.on('click', 'button', function () {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.special-list').isotope({
+			itemSelector: '.special-grid'
+		});
+	});
+	
+	/* ..............................................
+    BaguetteBox
+    ................................................. */
+	
+	baguetteBox.run('.tz-gallery', {
+		animation: 'fadeIn',
+		noScrollbars: true
+	});
+		
+}(jQuery));
